0a3f9459d9092d495cc40974573969f16fd15824
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2013-12-13 09:04:52 -0700

    Standardizing License

--- src/main/java/com/github/joakime/websocket/OnEchoMessage.java
+++ src/main/java/com/github/joakime/websocket/OnEchoMessage.java
@@ -1,6 +1,6 @@
 //
 //  ========================================================================
-//  Copyright (c) - Joakim Erdfelt
+//  Copyright (c) 2013 - Joakim Erdfelt
 //  ------------------------------------------------------------------------
 //  All rights reserved. This program and the accompanying materials
 //  are made available under the terms of the Eclipse Public License v1.0
